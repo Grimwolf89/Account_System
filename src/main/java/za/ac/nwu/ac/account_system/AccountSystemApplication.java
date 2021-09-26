e6441ed44a6afa509a8bcf2a18989bf1eeb5d8d6
Grimwolf89 <shaunlwc@gmail.com>
2021-09-26 15:34:06 +0200

    Database Entities Setup for AS

--- src/main/java/za/ac/nwu/ac/account_system/AccountSystemApplication.java
+++ src/main/java/za/ac/nwu/ac/account_system/AccountSystemApplication.java
@@ -2,12 +2,21 @@ package za.ac.nwu.ac.account_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class AccountSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AccountSystemApplication.class, args);
     }
 
+    @GetMapping
+    public List<String> hello() {
+        return List.of("Initial", "Testing", "Endpoint", "Resolved") ;
+    }
 }
